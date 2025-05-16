57ec1b9ac9c09e8e7bc60fde8aa4da527d4fbe82
thecong2006 <megame0113@gmail.com>
2025-05-17 01:26:58 +0700

    Day

---
+++ Assets/_Scripts/PlayfabManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+
+public class PlayfabManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Login();
+    }
+    private void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Tạo thành công");
+    }
+    void OnError(PlayFabError error)
+    {
+        Debug.Log(error);
+        Debug.Log(error.GenerateErrorReport());
+    }
+}
